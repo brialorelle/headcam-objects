4f0f38d6b46f6944e90e11fc3eedace9a4f8a063
naitib <naitibhatt@gmail.com>
2021-02-23 09:13:57 -0800

    remove other from interesting images

--- analysis/full_goldset/grabGoodFrames.R
+++ analysis/full_goldset/grabGoodFrames.R
@@ -15,7 +15,19 @@ load(file = here::here('headcam-objects/data/preprocessed_data/goldset_category_
 filtered_by_image <-  d %>%
   select(public_url, age_days, age_day_bin, child_id, category, conf) %>%
   spread(key = category, value=conf) %>% 
-  filter(is.na(Nothing))
+  filter(is.na(Nothing)) %>% 
+  filter(is.na(Other))
 
+interesting_image_urls <- filtered_by_image %>% 
+  select(public_url) %>% 
+  transmute(image_url = public_url)
 
+write.csv(interesting_image_urls, here::here('goldset_to_annotate.csv'), row.names = FALSE)
+
+# choose 500 random interesting images
+interesting_few <- interesting_image_urls %>% 
+  sample_n(500)
+write.csv(interesting_few, here::here('goldset_sample_annotate.csv'), row.names = FALSE)
+
+# read in category names
 dat = read.table(here::here("categories.txt"), sep="\n")
